be37f6f736f6eb446bb125aa958be5502e5c3dd7
Little_Teu <s_azarenko@outlook.com>
2020-12-27 01:42:08 +0300

    Загрузка простых тестовых заданий из БД.

--- www/scripts/test.js
+++ www/scripts/test.js
@@ -7,6 +7,15 @@ function doAfterCheckingLoginStatus(logined) {
     getQuestions().then(
         msg => {
             console.log(msg);
+            msg["questions"].forEach(
+                question => {
+                    appendQuestion(question, msg["answers"].filter(
+                        answer => {
+                            return answer["questionId"] === question["id"];
+                        }
+                    ));
+                }
+            );
         }
     ).catch(e => console.log(e));
 }
@@ -23,16 +32,62 @@ async function getQuestions(){
     );
 }
 
+function appendQuestion(questionObject, answerObjectsArray){
+    const questionId = questionObject["id"];
+    $("#testQuestions").append(
+        $("<div class=\"test-question container border border-dark mb-2\">")
+            .append(
+                $("<form>")
+                    .append(
+                        $("<h5 class=\"text-center\">")
+                            .text("Вопрос ".concat(questionId))
+                    )
+                    .append(
+                        $("<div class=\"questionQuestion\">")
+                            .text(questionObject["text"])
+                    )
+                    .append(
+                        $("<div id=\"questionAnswer-" + questionId + "\" class=\"container questionAnswer mb-2\" data-answer=\"\">")
+                            .append(
+                                $("<div class=\"row\">")
+                            )
+                    )
+            )
+    );
+
+    const answers = $("#questionAnswer-" + questionId + " .row");
+    if (answerObjectsArray.length > 0){
+        // Если вопрос с вариантами ответа:
+        answerObjectsArray.forEach((answer, index) => {
+            answers.append(
+                // Здесь ответы:
+                // Например:
+                $("<div>")
+                    .append(
+                        $("<input id=\"radio"+ questionId +"-" + index.toString() +"\" name=\"radio-1\" class=\"form-check-input\" type=\"radio\" data-answer-for=\"questionAnswer-"+ questionId +"\">")
+                            .val(answer["answer"])
+                    )
+                    .append(
+                        $("<label for=\"radio"+ questionId +"-" + index.toString() +"\" class=\"form-check-label\">")
+                            .text("\xa0" + answer["answer"])
+                    )
+            );
+        });
+    } else {
+        // Если вопрос без вариантов ответа:
+    }
+}
+
 $(() => {
     if (testId === null){
         $("#errorMissingTestId")[0].classList.remove('hidden');
     } else {
-        // checkUserLoginStatus()
-        //     .catch(e => console.log(e))
-        //     .then(
-        //         logined => {
-        //             doAfterCheckingLoginStatus(logined);
-        //         }
-        //     );
+        checkUserLoginStatus()
+            .catch(e => console.log(e))
+            .then(
+                logined => {
+                    doAfterCheckingLoginStatus(logined);
+                }
+            );
     }
 });
